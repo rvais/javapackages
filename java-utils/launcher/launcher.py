eb09bcd65d5eb87d2189cc26c8eade17ad5f4b41
Roman Vais <rvais@redhat.com>
2016-11-04 18:32:44 +0100

    Add verzion of launcher that is currently being debugged

--- java-utils/launcher/launcher.py
+++ java-utils/launcher/launcher.py
@@ -8,33 +8,36 @@ import sys              # argumenst accessed by  sys.argv
 import os               # environment variables of the shell/machine
 import errno            # errno for mkdirs function
 import configparser     # ini file parsing module
+import subprocess       # for launching another application
 
 # definiton of variables 
-cfgFile="jlauncher.cfg.ini" # default name of config file for this launcher
+cfgFile="jlauncher.cfg.ini"         # default name of config file for launcher
 globalCfg="jlauncher/global.ini"
-testEnvironment="$HOME/tstenv" # abs path to dir where to create test env
-                               # if it's not to be used leave empty string
+testEnvironment="$HOME/tstenv"      # abs path to dir where to create test env
+                                    # if it's not to be used leave empty str
 
 # dictionary containing setting for luncher that will be applied 
 cfg={"jvmbinary" : "java",    # JVM binary
      "jvmoptions" : "",       # JVM options
-     "applargs" : "",          # Default application arguments
-     "mainclass" : "",        # class path to main class of the application
+     "applargs" : "",         # Default application arguments
+     "mainclass" : "",        # Main class of the application
+     "classpath" : "",        # Claspath with individual JAR files
      "abrtflag" : "",         # ABRT integration
      "ignoreuserconfig" : ""  # flag "ignore user settings" (always uses app specific)
 }
 
 # name of the program we want to launch
 prog="program" 
+launcher="launcher.py" # original name of a launcher script
 
 # list of environment variables we are interested in
-envList=["HOME", "XDG_DATA_HOME", "XDG_CONFIG_HOME", "XDG_CACHE_HOME",
-         "XDG_CONFIG_DIRS", "XDG_DATA_DIRS"] # , ""
+envList=["HOME", "MAVEN_REPO" , "XDG_DATA_HOME", "XDG_CONFIG_HOME",
+         "XDG_CACHE_HOME", "XDG_CONFIG_DIRS", "XDG_DATA_DIRS"] # , ""
 
 # XDG defaults - default values for unset XDG environmental variables
 xdgDef={"XDG_DATA_HOME" : "$HOME/.local/share", "XDG_CONFIG_HOME" : "$HOME/.config",
         "XDG_CACHE_HOME" : "$HOME/.cache", "XDG_DATA_DIRS" : "/usr/local/share:/usr/share",
-        "XDG_CONFIG_DIRS" : "/etc/xdg"}
+        "XDG_CONFIG_DIRS" : "/etc/xdg", "MAVEN_REPO" : "$HOME/.m2/repository"}
 
 # values of environmental variables that we actualy use
 env={}
@@ -94,8 +97,18 @@ for key, value in env.items():
     value = ':'.join(paths)
     env[key] = value
 
+# get program neame form symbolic link
+arg0 = os.path.basename(sys.argv[0])
+print("'" + arg0 + "'")
 
-help()
+if (arg0 == launcher or arg0 == ''):
+    if (len(sys.argv) <= 1 or 
+        (len(sys.argv[1]) <= 0 or sys.argv[1].startswith('-'))):
+        help()
+        exit(1);
+    arg0 = os.path.basename(sys.argv[1])
+
+prog = arg0
 
 for key, value in env.items():
     print("'" + key + "' = '" + value + "'")
@@ -115,11 +128,12 @@ if (os.path.exists(filename) and os.path.isfile(filename)):
     iniParser.read_file(open(filename))
 
     for item, value in iniParser['section'].items():
-        cfg[item] = value
+        cfg[item] = value.strip('"')
 
-for item, value in cfg.items():
-    print(item + " = " + value)
+    for item, value in cfg.items():
+        print(item + " = " + value)
 
+# TODO: later implement search on other paths as well
 
 # try to locate program configuration
 
@@ -129,9 +143,32 @@ for item, value in cfg.items():
 
 # version=$(awk -F "=" '/database_version/ {print $2}' parameters.ini)
 
+# check if all necesarry configuration is available
+for item, value in cfg.items():
+    if (item.startswith('jvmb') or item.startswith('main') or item.startswith("classpath")):
+#    if (item.startswith('jvmb') or item.startswith("classpath")):
+        if (value == ''):
+            print("Configuration of '" + item + "' is missing for '" + 
+                prog + "' application.\n" +
+                "There for it is not possible to launch it.", file=sys.stderr)
+            exit(2)
 
+args = [cfg["jvmbinary"], cfg["jvmoptions"], "-classpath", cfg["classpath"], cfg["mainclass"],
+        cfg["applargs"]]
+if not ('SystemProperties' in iniParser):
+    print ("No system properties ... ")
+else:
+    for prop, value in iniParser['SystemProperties'].items():
+        args.insert(4,"-D" + prop + "=" + value)
 
+for string in args:
+    sys.stdout.write(string + " ")
+sys.stdout.write("\n")
+sys.stdout.flush()
 
+# exit(0)
+
+exit(subprocess.run(args).returncode)
 
 
 
